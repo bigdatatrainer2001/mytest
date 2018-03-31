16fef644b5a06cb6cf837513ee163530e06bcc8b
bigdatatrainer2001 <bigdatatrainer2001@sit.com>
2018-03-31 10:12:17 +0530

    first commit

---
+++ myfirstmvnproject/src/main/java/sittutorial/myfirstmvnproject/App.java
@@ -0,0 +1,13 @@
+package sittutorial.myfirstmvnproject;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
